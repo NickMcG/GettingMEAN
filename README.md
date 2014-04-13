640d0f96fd2ffa41004e40aab494030b8d3843e2
Nick McGinness <ragnerd@gmail.com>
2014-04-13 16:40:29 -0500

    Finishing up chapter 5

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 GettingMEAN
 ===========
+This is my workspace for working through the "Getting MEAN" book from Manning Publishing.
+To use this, you will need to set a node environment variable (MONGO_URI) to point to a mongo database.
--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,51 @@
 var mongoose = require('mongoose');
+var readline = require('readline');
+
+var dbURI = process.env.MONGO_URI;
+
+var gracefulShutdown = function (msg, callback) {
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	});
+};
+
+if (process.platform === 'win32') {
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	rl.on('SIGINT', function () {
+		process.emit('SIGINT');
+	});
+}
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+	console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+	console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+	console.log('Mongoose disconnected');
+});
+
+process.once('SIGUSR2', function () {
+	gracefulShutdown('nodemon restart', function () {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+process.on('SIGINT', function () {
+	gracefulShutdown('app termination', function () {
+		process.exit(0);
+	});
+});
+process.on('SIGTERM', function () {
+	gracefulShutdown('Heroku app shutdown', function () {
+		process.exit(0);
+	});
+});
+
+require('./locations');
